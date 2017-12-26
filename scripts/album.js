d7998c3a0ffa9551d2d1338e52b9b54e6a986cc8
Jon <jjsang13@gmail.com>
2017-12-26 15:53:06 -0500

    Assignment 24 fixed

--- scripts/album.js
+++ scripts/album.js
@@ -58,14 +58,13 @@
      return template;
  };
 
- var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
+ var setCurrentAlbum = function(album) {
      // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -80,16 +79,17 @@
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
- index = 1;
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
 
      var albums = [albumPicasso, albumMarconi, albumJon];
+     var index = 1;
 
-     albumImage.window.addEventListener('click', function(event) {
-       setCurrentAlbum(album[index]);
+     albumImage.addEventListener("click", function(event) {
+       setCurrentAlbum(albums[index]);
        index++;
-       if(index == album.length) {
+       if(index == albums.length) {
          index = 0;
        }
      });
